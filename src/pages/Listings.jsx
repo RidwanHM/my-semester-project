b3a76d58b1747fd17078c640394671585f93e32d
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-12-17 09:59:20 +0100

    fixed footer

--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,11 +1,13 @@
 import Navigation from "../components/navbar";
 import ListingDetail from "../components/listings";
+import Footer from "../components/footer";
 
 export default function ListingsPage() {
   return (
     <>
       <Navigation />
       <ListingDetail />
+      <Footer />
     </>
   );
 }
