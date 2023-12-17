a3ee6f78339e2de5a0cc8e502f19927471dd81ae
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-12-17 03:12:15 +0100

    password black field

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -122,7 +122,7 @@ function LoginForm() {
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-white"
+                  className="block text-sm font-medium leading-6 text-black"
                 >
                   Password
                 </label>
