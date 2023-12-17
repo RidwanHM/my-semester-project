623972a4f39055d5bdca03e5b6e2365cef86f944
RidwanHM <ridwan.h.mohamud@gmail.com>
2023-12-17 03:13:52 +0100

    push

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -136,7 +136,7 @@ function LoginForm() {
                   autoComplete="current-password"
                   required
                   defaultValue="UzI1NiIsInR5cCI"
-                  className="block w-full rounded-md border-0 px-1 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 px-1 py-1.5 text-white shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
               </div>
               {error.password && (
